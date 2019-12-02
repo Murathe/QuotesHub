a866d225a76ad5d924f9eac99220fe1901ac9ee0
murathe <isaacsm07p@gmail.com>
2019-12-02 14:29:05 +0300

    Add date find function

--- src/app/date-count.pipe.ts
+++ src/app/date-count.pipe.ts
@@ -4,9 +4,21 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'dateCount'
 })
 export class DateCountPipe implements PipeTransform {
+ 
+  transform(value: any): number {
+    let today:Date = new Date();
+    let todayWithNoTime:any = new Date(today.getFullYear(), today.getMonth(), today.getDate());
+    var dateDiff = Math.abs(value - todayWithNoTime);
+    const secondsInDay = 86400;
+    var dateDiffSec = dateDiff*0.001;
+    var dateCounter = dateDiffSec/secondsInDay;
 
-  transform(value: any, args?: any): any {
-    return null;
+    if (dateCounter >= 1 && value > todayWithNoTime) {
+      return dateCounter;
+    } else {
+      return 0;
+    }
+    
   }
 
 }
