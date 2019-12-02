ade49c95bb68434de5098bb895feeab424a70f0d
murathe <isaacsm07p@gmail.com>
2019-12-02 14:13:02 +0300

    Initialize addNewQuote event

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
+import { Quote } from './../quote';
+import { EventEmitter } from 'events';
 
 @Component({
   selector: 'app-quote-form',
@@ -6,6 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote-form.component.css']
 })
 export class QuoteFormComponent implements OnInit {
+
+  newQuote = new Quote(0, "", "", "", new Date(), 0,0);
+
+  @Output() addQuote = new EventEmitter<Quote>();
+
+  addNewQuote: any;
+
 
   constructor() { }
 
