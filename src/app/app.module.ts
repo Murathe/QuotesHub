bbe6094e6b5319d69953da20681c337938bc6e86
murathe <isaacsm07p@gmail.com>
2019-12-02 11:04:48 +0300

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { QuoteComponent } from './quote/quote.component';
+import { QuoteDetailsComponent } from './quote-details/quote-details.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { HighlightPipe } from './highlight.pipe';
+import { DateCountPipe } from './date-count.pipe';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    QuoteComponent,
+    QuoteDetailsComponent,
+    QuoteFormComponent,
+    HighlightPipe,
+    DateCountPipe
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
